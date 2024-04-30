59d881f434c4fbc9d13a8179911873f1b410c06a
Ajandro <adelaygleisafleitas@gmail.com>
2024-04-29 20:47:11 -0400

    1er comit

---
+++ script.js
@@ -0,0 +1,29 @@
+const buttonAvatar = document.getElementById("button_avatar");
+const buttonShare = document.getElementById("button_share");
+const contShare = document.getElementById("cont_share");
+
+
+
+buttonAvatar.addEventListener("click", () => {
+  const width = window.innerWidth;
+  if (width >= 1440) {
+    if (contShare.classList.contains("hidden")) {
+      contShare.classList.remove("hidden");
+      contShare.classList.add("container__shareJS");
+      buttonShare.classList.add("hidden");
+    } else {
+      contShare.classList.add("hidden");
+    }
+  } else if (width < 1440) {
+    contShare.classList.remove("container__shareJS");
+    contShare.classList.remove("hidden");
+    buttonShare.classList.remove("hidden");
+  }
+});
+
+buttonShare.addEventListener("click", () => {
+  const width = window.innerWidth;
+  if (width < 1440) {
+    contShare.classList.add("hidden");
+  }
+});
